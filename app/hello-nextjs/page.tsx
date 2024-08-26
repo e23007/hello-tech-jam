a498cb2003073d4daef84db48adfc4850468caba
e23007 <e23007@std.it-college.ac.jp>
2024-08-26 17:44:25 +0900

    ホットペッパーAPI追加

--- app/hello-nextjs/page.tsx
+++ app/hello-nextjs/page.tsx
@@ -1,5 +1,9 @@
 export default function Page() {
+    const mybar="hoge"
+    const mybat="hoge"
     return (
-      <div>Hello, Next.js</div>
+                <div>
+        Hello, Next.js
+</div>
     )
   }
